c4535c034cade87529766286eb1f1140675fc8ff
uo271080 <plannetoficial@gmail.com>
2024-06-17 20:58:33 +0200

    serialization/deserialization data api working

--- assembly-rdf-shape/src/app/api.rs
+++ assembly-rdf-shape/src/app/api.rs
@@ -1,7 +1,7 @@
 // src/api.rs
 use reqwasm::http::Request;
 use wasm_bindgen_futures::spawn_local;
-use web_sys::console;
+use web_sys::{console, js_sys::JsString};
 
 // src/models.rs
 use serde::{Deserialize, Serialize};
@@ -45,7 +45,33 @@ pub struct RequestBody {
     pub trigger_mode: TriggerMode,
 }
 
-pub fn createRequestBody(rdf_content:String,shex_content:String,shapemap_content:String) -> RequestBody{
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct ValidationResult {
+    pub result:Result
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct Result{
+    pub valid: bool,
+    pub message: String,
+    #[serde(rename = "shapeMap")]
+    pub shape_map: Vec<ShapeMapEntry>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct ShapeMapEntry {
+    pub node: String,
+    pub shape: String,
+    pub status: String,
+    pub reason: Option<String>,
+}
+
+
+
+pub fn create_request_body(rdf_content:String,shex_content:String,shapemap_content:String) -> RequestBody{
     let data = Data {
         content: rdf_content,
         source: "byText".to_string(),
@@ -86,21 +112,21 @@ pub fn createRequestBody(rdf_content:String,shex_content:String,shapemap_content
 
 pub async fn call_validation_api(rdf_content:String,shex_content:String,shapemap_content:String) {
 
-    let request_body = createRequestBody(rdf_content, shex_content, shapemap_content);
+    let request_body = create_request_body(rdf_content, shex_content, shapemap_content);
     
     let validation_endpoint = "https://api.rdfshape.weso.es/api/schema/validate";
     let request_body_json = serde_json::to_string(&request_body).unwrap();
 
     spawn_local(async move {
-        let validation_result = Request::post(validation_endpoint)
+        let validation_result:ValidationResult = Request::post(validation_endpoint)
             .body(request_body_json)
             .send()
             .await
             .unwrap()
-            .text()
+            .json()
             .await
             .unwrap();
         
-        console::log_1(&validation_result.into());
+            console::log_1(&JsString::from(serde_json::to_string(&validation_result).unwrap()));
     });
 }
