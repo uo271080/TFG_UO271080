aa31e5dd1179b802a0cdad988831a7db8a7ce8f7
uo271080 <plannetoficial@gmail.com>
2024-07-04 12:57:53 +0200

    prepare to deploy

--- assembly-rdf-shape/src/app/examples_manager.rs
+++ assembly-rdf-shape/src/app/examples_manager.rs
@@ -33,15 +33,14 @@ pub struct ExampleData {
 /// * Retorna un error si la solicitud HTTP no se puede completar o si el estado HTTP no es 200.
 /// * Retorna un error si la deserialización del JSON falla.
 pub async fn load_example(file: String) -> Result<ExampleData, String> {
-    // let mut public_url = env::var("https://uo271080.github.io/TFG_UO271080/")
-    //     .unwrap_or("https://uo271080.github.io/TFG_UO271080/".to_string());
-    // if !public_url.ends_with('/') {
-    //     public_url.push('/');
-    // }
+    let mut public_url = env::var("https://uo271080.github.io/TFG_UO271080/")
+        .unwrap_or("https://uo271080.github.io/TFG_UO271080/".to_string());
+    if !public_url.ends_with('/') {
+        public_url.push('/');
+    }
 
-    // let path = format!("{}static/{}.json", public_url, file);
+    let path = format!("{}static/{}.json", public_url, file);
 
-    let path = format!("/static/{}.json", file);
     let response = Request::get(&path)
         .send()
         .await
